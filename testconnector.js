02c92552118454809e4c7edfdf0ec327d1042f98
Haris Rahim <haris.rahim@AUSTRA-MBP-HRahim.local>
2023-10-11 12:37:13 +1100

    add files

---
+++ testconnector.js
@@ -0,0 +1,22 @@
+// For the default version
+// import algoliasearch from 'algoliasearch';
+
+// const client = algoliasearch('', '');
+// const index = client.initIndex('');
+
+// console.debug(await index.getSettings())
+
+// try {
+//     await index.searchForFacetValues('', '').then(({ facetHits }) => {
+//         console.log(facetHits);
+//     })
+// } catch (e) {
+//     console.error(e);
+// }
+
+// // Search the "category" facet for values matching "phone" in records
+// // having "Apple" in their "brand" facet.
+// index.searchForFacetValues('framing', 'close').then(({ facetHits }) => {
+//     console.log(facetHits);
+//   });
+
